eff5bad416f12d9a5b2c3da01194d6930eb32642
alex14-byte <85235998+alex14-byte@users.noreply.github.com>
2025-03-13 21:18:38 +0500

    Сделала задание

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def summ (a, b):
+    return a+b
+
+def diff (a, b):
+    return a-b
+
+def delenie (a, b):
+    if b == 0:
+        return ('На ноль делить нельзя!')
+    else:
+        return a/b
+
+def umnagenie (a, b):
+    return a*b
---
+++ main.py
@@ -0,0 +1,19 @@
+from calculator import *
+print('Привет! Этот калькулятор ты можешь использовать для сложения, вычитания, деления и умножения.')
+
+while True:
+    op = input("Введи операцию (сложение, вычитание, деление и умножение) или 'стоп' для остановки: ")
+    if op == 'стоп':
+        break
+    a = int(input("Введи первое число: "))
+    b = int(input("Введи второе число: "))
+    if op == 'сложение':
+        print(summ(a, b))
+    elif op == 'вычитание':
+        print(diff(a, b))
+    elif op == 'деление':
+        print(delenie(a, b))
+    elif op == 'умножение':
+        print(umnagenie(a, b))
+
+
